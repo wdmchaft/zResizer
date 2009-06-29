0a8e1be53a942005cda0b0f68d4a4d695d99e598
Zettt <info@zettt.de>
2009-06-29 21:52:14 +0200

    Initial commit.

---
+++ AZController.h
@@ -0,0 +1,16 @@
+//
+//  AZController.h
+//  Rszr
+//
+//  Created by Zettt on 29.06.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AZController : NSObject {
+
+}
+
+@end
---
+++ AZController.m
@@ -0,0 +1,14 @@
+//
+//  AZController.m
+//  Rszr
+//
+//  Created by Zettt on 29.06.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "AZController.h"
+
+
+@implementation AZController
+
+@end
